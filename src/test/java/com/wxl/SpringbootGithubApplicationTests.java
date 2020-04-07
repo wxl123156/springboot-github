0168e33e286a51baad0b9e287bd2c57485932aa0
LeeHappy <happy@git.com>
2020-04-07 16:57:10 +0800

    111

---
+++ src/test/java/com/wxl/SpringbootGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
