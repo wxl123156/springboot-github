79b02aa4b5a284059e2a8627898127308bc4154a
LeeHappy <happy@git.com>
2020-04-10 08:53:52 +0800

    修改了启动类

--- src/main/java/com/wxl/SpringbootGithubApplication.java
+++ src/main/java/com/wxl/SpringbootGithubApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootGithubApplication {
-
+	
     public static void main(String[] args) {
+	    System.out.println("hello world");
         SpringApplication.run(SpringbootGithubApplication.class, args);
     }
 
